76fceeac4dec9006aac946b58012e9aeae66e96f
Rupesh Raundal <rupeshraundal@Rupeshs-MacBook-Pro.local>
2017-04-09 14:32:16 +0530

    Extra route for locations commented

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
  Rails.application.routes.draw do
-  resources :users do
-    resources :locations
-  end
+#  resources :users do
+#    resources :locations
+#  end
 #  resources :users do
 #    resources :subscriptions
 #  end
