b9bf57ea2e5af0891a53cab8eb12dfbdc2b5911b
prashantsunoray <prashant.chaudhari@sunoray.com>
2017-04-14 21:59:37 +0530

    Added payment redirect .

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -51,7 +51,7 @@ class PaymentsController < ApplicationController
 
     respond_to do |format|
       if @payment.save
-        format.html { redirect_to @payment, notice: 'Payment was successfully created.' }
+        format.html { redirect_to user_subscriptions_path(current_user), notice: 'Payment was successfully created.' }
         format.json { render :show, status: :created, location: @payment }
       else
         format.html { render :new }
