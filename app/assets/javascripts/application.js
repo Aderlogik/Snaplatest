7dc0b40d22b1bdb9379a6e4e9bb64e44cb3b948f
Rupesh Raundal <rupesh@zerebral.co.in>
2017-05-18 08:50:41 +0530

    Signup validation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,27 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+function signup_validation(){
+    var is_valid = true;
+    
+    //blank fields validation
+    $("#new_user input[type='text'], #new_user input[type='password']").each(function( index ) {
+       if($(this).val() === ""){
+           $(this).css("border", "solid 1px red");
+           is_valid = false;
+       }
+    });
+    
+    //password and confirm password validation
+    var password = $("#user_password").val();
+    var confirm_password = $("#user_confirm_password").val();
+    if(password !== confirm_password){
+        alert("Passwords do not match.");
+        is_valid = false;
+    }
+    
+    if(!is_valid){
+        return false;
+    }
+}
