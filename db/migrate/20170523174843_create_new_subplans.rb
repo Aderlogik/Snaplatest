7d13674802ffbc79863e2692092d73466ca9a65b
Rupesh Raundal <rupesh@zerebral.co.in>
2017-05-23 23:19:27 +0530

    Subplan migration

---
+++ db/migrate/20170523174843_create_new_subplans.rb
@@ -0,0 +1,12 @@
+class CreateNewSubplans < ActiveRecord::Migration[5.0]
+  def change
+    create_table :subplans do |t|
+      t.string :name
+      t.integer :price
+      t.integer :plan_id
+      t.integer :services_commited
+
+      t.timestamps
+    end
+  end
+end
